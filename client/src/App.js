4ecd518e521f516e03e76676725cd19f4bcf766f
omiinaya <omiinaya@gmail.com>
2021-08-29 19:06:49 -0400

    implemented home page route.

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import Home from "./components/Home/";
+import Default from "./components/Default/";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/default" component={Default} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ client/src/components/Home/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <div>Hello World.</div>
+                <a href="/default">Route test.</a>
+            </div>
+        )
+    }
+}
+
+export default Home;

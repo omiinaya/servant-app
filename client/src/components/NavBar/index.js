422ae962d589ffb59803fd141fe1962082bef131
omiinaya <omiinaya@gmail.com>
2021-09-01 22:15:58 -0400

    prepared navbar to allow conditonal rendering.

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
     Collapse,
     Navbar,
@@ -10,41 +10,50 @@ import {
 import PopUp from "../PopUp/"
 import Login from "../Login"
 import Register from "../Register"
+//import { isLoggedIn } from './scripts';
 
-const NavBar = () => {
+class NavBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: false,
+        };
+    }
 
-    const [isOpen, setIsOpen] = useState(false);
+    toggle() {
+        this.setState({ isOpen: !this.state.isOpen })
+    }
 
-    const toggle = () => setIsOpen(!isOpen);
-
-    return (
-        <div>
-            <Navbar color="light" light expand="md">
-                <NavbarBrand href="/">ServantApp</NavbarBrand>
-                <NavbarToggler onClick={toggle} />
-                <Collapse isOpen={isOpen} navbar>
-                    <Nav className="justify-content-end" style={{ width: "100%" }}>
-                        <NavItem>
-                        <PopUp
-                                buttonLabel='Sign In'
-                                content={<Login />}
-                                title='Sign In'
-                                color="link"
-                            />
-                        </NavItem>
-                        <NavItem>
-                            <PopUp
-                                buttonLabel='Join'
-                                content={<Register />}
-                                title='Sign Up'
-                                color="secondary"
-                            />
-                        </NavItem>
-                    </Nav>
-                </Collapse>
-            </Navbar>
-        </div>
-    );
+    render() {
+        return (
+            <div>
+                <Navbar color="light" light expand="md">
+                    <NavbarBrand href="/">ServantApp</NavbarBrand>
+                    <NavbarToggler onClick={ ()=> { this.toggle() } } />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="justify-content-end" style={{ width: "100%" }}>
+                            <NavItem>
+                                <PopUp
+                                    buttonLabel='Sign In'
+                                    content={<Login />}
+                                    title='Sign In'
+                                    color="link"
+                                />
+                            </NavItem>
+                            <NavItem>
+                                <PopUp
+                                    buttonLabel='Join'
+                                    content={<Register />}
+                                    title='Sign Up'
+                                    color="secondary"
+                                />
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        );
+    }
 }
 
 export default NavBar;
